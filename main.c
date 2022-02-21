7f78e4e1fac66bea8665d59d05d4bdbe29c07032
Amanda <97198491+amandaumlauf@users.noreply.github.com>
2022-02-21 09:36:19 -0300

    Update and rename AmandaUmlauf_JoãoVictorCampregher _Trabalho_Final.c to main.c

--- main.c
+++ main.c
@@ -1,35 +1,34 @@
-/*Alunos: Amanda Umlauf e João Victor Campregher
-Data: 19/02/2022
-Função: O programa serve como uma forma do usuário controlar seu acervo pessoal de livros. A partir do menu, o usuário consegue cadastrar novos livros, consultar os livros que já possui, excluir e alterar.*/
+/*FunÃ§Ã£o: O programa serve como uma forma do usuÃ¡rio controlar seu acervo pessoal de livros. 
+A partir do menu, o usuÃ¡rio consegue cadastrar novos livros, consultar os livros que jÃ¡ possui, excluir e alterar.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 
-struct Livro //usamos struct para criar uma estrurura que armazena diversas variáveis e seus dados em uma variavel especial
+struct Livro //usamos struct para criar uma estrurura que armazena diversas variÃ¡veis e seus dados em uma variavel especial
 {
     int id; 				//
     char nome[50];			//
-    char autor[50];			//declaração de variáveis da struct
+    char autor[50];			//declaraÃ§Ã£o de variÃ¡veis da struct
     char status[100];		//
     int pags;				//
 };
 
-struct Livro obra; //declaração de uma variável do tipo livro 
+struct Livro obra; //declaraÃ§Ã£o de uma variÃ¡vel do tipo livro 
 FILE *pArq; // apontando para o arquivo
 int tam; 
 
 void menu();                
 int cadastrar();
 void visualizar();
-void pesquisar();           //declaração de funções
+void pesquisar();           //declaraÃ§Ã£o de funÃ§Ãµes
 void excluirCadastro();
 void alterarCadastro();
 struct Livro* loadVet();
 
 int main(){ 
-    setlocale(LC_ALL, "Portuguese"); //setando a linguagem do programa para português
+    setlocale(LC_ALL, "Portuguese"); //setando a linguagem do programa para portuguÃªs
     menu();
 
     return 0;
@@ -38,24 +37,24 @@ int main(){
 void menu(){
     int opcao;
 
-    while (opcao != 6) { //enquanto opçao for diferente de 6 o menu ficam em loop de repetição
+    while (opcao != 6) { //enquanto opÃ§ao for diferente de 6 o menu ficam em loop de repetiÃ§Ã£o
 
         printf("\n_________\n");    
 	    printf("BIBLIOTECA PARTICULAR\n");
         printf("_______\n");
         printf("\n");
         printf("O QUE DESEJA FAZER?\n");
-        printf("SELECIONE UMA OPÇÃO:\n");
+        printf("SELECIONE UMA OPÃ‡ÃƒO:\n");
         printf("\n");
         printf("1. ADICIONAR UM LIVRO NOVO\n");
         printf("2. MOSTRAR LIVROS CADASTRADOS\n");
-        printf("3. PESQUISA POR NÚMERO DO LIVRO\n");
+        printf("3. PESQUISA POR NÃšMERO DO LIVRO\n");
         printf("4. EXCLUIR CADASTRO\n");
         printf("5. ALTERAR CADASTRO\n");
         printf("6. SAIR\n");
         scanf("%d", &opcao);
 
-        switch (opcao) { //estrutura condicional do tipo "switch case" para coordenar as opções de execução do programa
+        switch (opcao) { //estrutura condicional do tipo "switch case" para coordenar as opÃ§Ãµes de execuÃ§Ã£o do programa
     
             case 1:
                 cadastrar();
@@ -86,19 +85,19 @@ void menu(){
 int cadastrar(){
     struct Livro* vet=loadVet();
     int i;
-    if(tam == -1) //verifica a existência do arquivo
+    if(tam == -1) //verifica a existÃªncia do arquivo
       printf("Criando arquivo...\n");
     pArq = fopen("biblioteca.txt", "a"); //abre ou cria o arquivo txt
     if(pArq == NULL){
         printf("Erro na abertura do arquivo!"); //verifica se o arquivo abriu corretamente
         exit (1);
     }else{ //se abriu, continua
-        printf("INSIRA O NÚMERO DO LIVRO: ");
+        printf("INSIRA O NÃšMERO DO LIVRO: ");
         scanf("%d", &obra.id);
         fflush(stdin);
-        for(i=0;i<tam;i++) //para vetor até o fim do arquivo
-          if(vet[i].id == obra.id){ //verifica se existe cadastro com o mesmo número
-            printf("Já existe uma obra com este id.\n"); //caso exista, interrompe o cadastro
+        for(i=0;i<tam;i++) //para vetor atÃ© o fim do arquivo
+          if(vet[i].id == obra.id){ //verifica se existe cadastro com o mesmo nÃºmero
+            printf("JÃ¡ existe uma obra com este id.\n"); //caso exista, interrompe o cadastro
             return 0;
           }
         //inicia o cadastro
@@ -110,46 +109,46 @@ int cadastrar(){
         scanf(" %[^\n]",obra.autor);
         fflush(stdin);
   
-        printf("INSIRA O NÚMERO DE PÁGINAS: ");
+        printf("INSIRA O NÃšMERO DE PÃGINAS: ");
         scanf("%d", &obra.pags);
         fflush(stdin);
   
-        printf("VOCÊ JÁ LEU ESSE LIVRO? (INSIRA NO FORMATO: LIDO/LEITURA PENDENTE) ");
+        printf("VOCÃŠ JÃ LEU ESSE LIVRO? (INSIRA NO FORMATO: LIDO/LEITURA PENDENTE) ");
         scanf(" %[^\n]",obra.status);
         fflush(stdin);
 
-        fprintf(pArq, "Número: %d\n", obra.id);//imprime as variáveis no arquivo
-        fprintf(pArq, "Título: %s\n", obra.nome);
+        fprintf(pArq, "NÃºmero: %d\n", obra.id);//imprime as variÃ¡veis no arquivo
+        fprintf(pArq, "TÃ­tulo: %s\n", obra.nome);
         fprintf(pArq, "Nome do autor: %s\n", obra.autor);
-        fprintf(pArq, "Número de páginas: %d\n", obra.pags);
+        fprintf(pArq, "NÃºmero de pÃ¡ginas: %d\n", obra.pags);
         fprintf(pArq, "Status de leitura: %s\n", obra.status);
 
         fclose(pArq); //fecha o arquivo
     }
-    free(vet); //desaloca o espaço na memória do vet
+    free(vet); //desaloca o espaÃ§o na memÃ³ria do vet
     return 1; 
 } 
 
 void visualizar(){
   struct Livro *vet = loadVet();
   if(tam == 0 || tam == -1) //verifica se existem livros cadastrados (setado no struct Livro* loadVet())
-    printf("Não existem livros cadastrados.\n");
+    printf("NÃ£o existem livros cadastrados.\n");
   else{
   	pArq = fopen("biblioteca.txt", "r");
       if(pArq == NULL){
           printf("Erro na abertura do arquivo!");
           exit (1);
       }else{
-     		while(!feof (pArq)){ 		//lê o arquivo até o final
-            	fscanf(pArq, "Número: %d\n", &obra.id);  	//faz a leitura das variáveis no arquivo
-              	fscanf(pArq, "Título: %[^\n]\n", obra.nome);
+     		while(!feof (pArq)){ 		//lÃª o arquivo atÃ© o final
+            	fscanf(pArq, "NÃºmero: %d\n", &obra.id);  	//faz a leitura das variÃ¡veis no arquivo
+              	fscanf(pArq, "TÃ­tulo: %[^\n]\n", obra.nome);
               	fscanf(pArq, "Nome do autor: %[^\n]\n", obra.autor);
-              	fscanf(pArq, "Número de páginas: %d\n", &obra.pags);
+              	fscanf(pArq, "NÃºmero de pÃ¡ginas: %d\n", &obra.pags);
               	fscanf(pArq, "Status de leitura: %[^\n]\n", obra.status);
-  		    	printf("\nNúmero: %d\n", obra.id); 	 //imprime na tela 
-              	printf("Título: %s\n", obra.nome);
+  		    	printf("\nNÃºmero: %d\n", obra.id); 	 //imprime na tela 
+              	printf("TÃ­tulo: %s\n", obra.nome);
               	printf("Nome do autor: %s\n", obra.autor);
-              	printf("Número de páginas: %d\n", obra.pags);
+              	printf("NÃºmero de pÃ¡ginas: %d\n", obra.pags);
               	printf("Status de leitura: %s\n", obra.status);
           }
       }
@@ -167,21 +166,21 @@ void pesquisar(){
         printf("Erro na abertura do arquivo!");
         exit (1);
     }else{
-        printf("Informe o número do livro que deseja consultar:");
+        printf("Informe o nÃºmero do livro que deseja consultar:");
         scanf("%d", &id);
 
         while(!feof (pArq)){
-            fscanf(pArq, "Número: %d\n", &obra.id);
-            fscanf(pArq, "Título: %[^\n]\n", obra.nome);
+            fscanf(pArq, "NÃºmero: %d\n", &obra.id);
+            fscanf(pArq, "TÃ­tulo: %[^\n]\n", obra.nome);
             fscanf(pArq, "Nome do autor: %[^\n]\n", obra.autor);
-            fscanf(pArq, "Número de páginas: %d\n", &obra.pags);
+            fscanf(pArq, "NÃºmero de pÃ¡ginas: %d\n", &obra.pags);
             fscanf(pArq, "Status de leitura: %[^\n]\n", obra.status);
             
             if (id == obra.id){ //compara o id informado com o lido e imprime
-                printf("Número: %d\n", obra.id);
-                printf("Título: %s\n", obra.nome);
+                printf("NÃºmero: %d\n", obra.id);
+                printf("TÃ­tulo: %s\n", obra.nome);
                 printf("Nome do autor: %s\n", obra.autor);
-                printf("Número de páginas: %d\n", obra.pags);
+                printf("NÃºmero de pÃ¡ginas: %d\n", obra.pags);
                 printf("Status de leitura: %s\n", obra.status);
             }
         }
@@ -196,10 +195,10 @@ void excluirCadastro(){
     struct Livro* vet = loadVet();
 
     if(tam == 0 || tam == -1){
-      printf("Não existem livros cadastrados.\n");
+      printf("NÃ£o existem livros cadastrados.\n");
     }
     else{
-      printf("Informe o número do livro que deseja excluir:");
+      printf("Informe o nÃºmero do livro que deseja excluir:");
       scanf("%d", &codigo);
       
       pArq = fopen("biblioteca.txt", "w");
@@ -207,12 +206,12 @@ void excluirCadastro(){
           printf("Erro na abertura do arquivo!");
           exit (1);
       }else{
-          for(i = 0; i < tam; i++){ //para vetor até o fim do arquivo
+          for(i = 0; i < tam; i++){ //para vetor atÃ© o fim do arquivo
             if(vet[i].id != codigo){ //se id for diferente do informado ele escreve no arquivo
-              fprintf(pArq, "Número: %d\n", vet[i].id);
-              fprintf(pArq, "Título: %s\n", vet[i].nome);
+              fprintf(pArq, "NÃºmero: %d\n", vet[i].id);
+              fprintf(pArq, "TÃ­tulo: %s\n", vet[i].nome);
               fprintf(pArq, "Nome do autor: %s\n", vet[i].autor);
-              fprintf(pArq, "Número de páginas: %d\n", vet[i].pags);
+              fprintf(pArq, "NÃºmero de pÃ¡ginas: %d\n", vet[i].pags);
               fprintf(pArq, "Status de leitura: %s\n", vet[i].status);
             }
             else
@@ -222,8 +221,8 @@ void excluirCadastro(){
       fclose(pArq);
     }
     free(vet);
-    if(aux == 0) //se não for encontrado o id, e portanto aux não for setado como 1, imprime livro não encontrado
-      printf("Livro não encontrado.\n");
+    if(aux == 0) //se nÃ£o for encontrado o id, e portanto aux nÃ£o for setado como 1, imprime livro nÃ£o encontrado
+      printf("Livro nÃ£o encontrado.\n");
 }
 
 void alterarCadastro(){
@@ -231,36 +230,36 @@ void alterarCadastro(){
   int id,i, aux = 0;
 
   if(tam == 0 || tam == -1)
-      printf("Não existem livros cadastrados.\n");
+      printf("NÃ£o existem livros cadastrados.\n");
   else{
-    printf("Informe o número do livro que deseja editar:");
+    printf("Informe o nÃºmero do livro que deseja editar:");
     scanf("%i",&id);
 
     for(i = 0; i < tam; i++){ //percorre todos os cadastros
       if(vet[i].id == id){ //se id for igual ao informado
-        printf("\nInformações atuais do livro:\n"); //imprime informações encontradas 
-        printf("Título: %s\n", vet[i].nome);
+        printf("\nInformaÃ§Ãµes atuais do livro:\n"); //imprime informaÃ§Ãµes encontradas 
+        printf("TÃ­tulo: %s\n", vet[i].nome);
         printf("Nome do autor: %s\n", vet[i].autor);
-        printf("Número de páginas: %d\n", vet[i].pags);
+        printf("NÃºmero de pÃ¡ginas: %d\n", vet[i].pags);
         printf("Status de leitura: %s\n", vet[i].status);
         
-        printf("\nINSIRA O NOME DO LIVRO: "); //solicita novas informações para as variáveis
+        printf("\nINSIRA O NOME DO LIVRO: "); //solicita novas informaÃ§Ãµes para as variÃ¡veis
         scanf(" %[^\n]",obra.nome);
         fflush(stdin);
         printf("INSIRA O NOME DO AUTOR: ");
         scanf(" %[^\n]",obra.autor);
         fflush(stdin);
-        printf("INSIRA O NÚMERO DE PÁGINAS: ");
+        printf("INSIRA O NÃšMERO DE PÃGINAS: ");
         scanf("%d", &obra.pags);
         fflush(stdin);
-        printf("VOCÊ JÁ LEU ESSE LIVRO? (INSIRA NO FORMATO: LIDO/LEITURA PENDENTE) ");
+        printf("VOCÃŠ JÃ LEU ESSE LIVRO? (INSIRA NO FORMATO: LIDO/LEITURA PENDENTE) ");
         scanf(" %[^\n]",obra.status);
         fflush(stdin);
         aux = 1; //seta aux
         }
     }
 
-    if(aux == 1){ //se informações novas cadastradas (aux = 1 setado)
+    if(aux == 1){ //se informaÃ§Ãµes novas cadastradas (aux = 1 setado)
       pArq = fopen("biblioteca.txt", "w");
       if(pArq == NULL){
         printf("Erro na abertura do arquivo!");
@@ -268,30 +267,30 @@ void alterarCadastro(){
       }else{
         for(i = 0; i < tam; i++){
           if(vet[i].id == id){ //se id for igual ao informado
-            fprintf(pArq, "Número: %d\n", vet[i].id); //imprime informações no arquivo, id, vomo vet e demais como obra (informadas acima)
-            fprintf(pArq, "Título: %s\n", obra.nome);
+            fprintf(pArq, "NÃºmero: %d\n", vet[i].id); //imprime informaÃ§Ãµes no arquivo, id, vomo vet e demais como obra (informadas acima)
+            fprintf(pArq, "TÃ­tulo: %s\n", obra.nome);
             fprintf(pArq, "Nome do autor: %s\n", obra.autor);
-            fprintf(pArq, "Número de páginas: %d\n", obra.pags);
+            fprintf(pArq, "NÃºmero de pÃ¡ginas: %d\n", obra.pags);
             fprintf(pArq, "Status de leitura: %s\n", obra.status);
             }else{ //para ids diferentes
-              fprintf(pArq, "Número: %d\n", vet[i].id); //salva informações no arquivo como vet (originais)
-              fprintf(pArq, "Título: %s\n", vet[i].nome);
+              fprintf(pArq, "NÃºmero: %d\n", vet[i].id); //salva informaÃ§Ãµes no arquivo como vet (originais)
+              fprintf(pArq, "TÃ­tulo: %s\n", vet[i].nome);
               fprintf(pArq, "Nome do autor: %s\n", vet[i].autor);
-              fprintf(pArq, "Número de páginas: %d\n", vet[i].pags);
+              fprintf(pArq, "NÃºmero de pÃ¡ginas: %d\n", vet[i].pags);
               fprintf(pArq, "Status de leitura: %s\n", vet[i].status);
           }
         }
         fclose(pArq);
       }
     }
-    else //se aux = 0, livro não encontrado
-      printf("Livro não encontrado.\n"); 
+    else //se aux = 0, livro nÃ£o encontrado
+      printf("Livro nÃ£o encontrado.\n"); 
   }
   free(vet);
 }
 
 
-struct Livro* loadVet(){ // essa função aloca um vetor dinamicamente conforme a quantidade de registros tem no arquivo e depois retorna esse vetor com todos os dados contidos no arquivo
+struct Livro* loadVet(){ // essa funÃ§Ã£o aloca um vetor dinamicamente conforme a quantidade de registros tem no arquivo e depois retorna esse vetor com todos os dados contidos no arquivo
   struct Livro *vet = NULL; //seta vet como null
   tam = 0; 
 
@@ -301,12 +300,12 @@ struct Livro* loadVet(){ // essa função aloca um vetor dinamicamente conforme a 
         tam = -1; //se o arquivo retornar null, seta tam = -1
     }else{
       while(!feof (pArq)){
-            fscanf(pArq, "Número: %s\n", obra.nome); //leio como string pois fica uma linha em branco no final do arquivo
+            fscanf(pArq, "NÃºmero: %s\n", obra.nome); //leio como string pois fica uma linha em branco no final do arquivo
             if(!feof(pArq)){
               obra.id = atoi(obra.nome); //transforma string em inteiro
-              fscanf(pArq, "Título: %[^\n]\n", obra.nome);
+              fscanf(pArq, "TÃ­tulo: %[^\n]\n", obra.nome);
               fscanf(pArq, "Nome do autor: %[^\n]\n", obra.autor);
-              fscanf(pArq, "Número de páginas: %d\n", &obra.pags);
+              fscanf(pArq, "NÃºmero de pÃ¡ginas: %d\n", &obra.pags);
               fscanf(pArq, "Status de leitura: %[^\n]\n", obra.status);
               tam++; //cada vez que roda conta +1
               vet = realloc(vet,sizeof(struct Livro) * tam); //realoca o vetor
